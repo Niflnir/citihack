cde7c4e0653c61e5335fafeaddc30850d6a52db9
RyanCodesHard <ryan.taw96@gmail.com>
2022-08-09 18:22:19 +0800

    Initial commit for myinvestment screen

--- web/my-app/src/pages/Quiz.js
+++ web/my-app/src/pages/Quiz.js
@@ -33,20 +33,118 @@ const Quiz = () => {
         display: "flex",
         justifyContent: "center"  
       }}>
-        <Grid spacing={3}>
-          <div>
-            <Typography>
+        <Grid direction="column" container spacing={2}>
+          <Grid alignSelf="center" 
+            item 
+            sx={{
+              my: "30px",
+            }}
+          >
+            <Typography fontSize={23}>
               Portfolio Questionaire
             </Typography>
-          </div>
-          <div>
+          </Grid>
+          <Grid item
+            sx={{
+              ml: "30px",
+              my: "17px"
+            }}
+          >
             <TextField 
               required
               id="outlined-required"
               label="Required"
-              defaultValue="Hello World"
+              helperText="Please enter your monthly income"
+              variant="standard"
+              value={income}
+              onChange={e => setIncome(e.target.value)}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    $
+                  </InputAdornment>
+                ),
+              }}
             />
-          </div>
+          </Grid>
+          <Grid item
+            sx={{
+              ml: "30px",
+              my: "17px"
+            }}
+          >
+            <TextField 
+              required
+              id="outlined-required"
+              label="Required"
+              helperText="Please enter the % you willing to invest"
+              variant="standard"
+              value={percentIncome}
+              onChange={e => setPercentIncome(e.target.value)}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    %
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+          <Grid item 
+            sx={{
+              ml: "30px",
+              my: "17px"
+            }}
+          >
+            <FormControl>
+              <InputLabel>Risk Index</InputLabel>
+              <Select
+                value={risk}
+                label="Risk Index"
+                onChange={e => setRisk(e.target.value)}
+              >
+                <MenuItem value={"low"}>Low</MenuItem>
+                <MenuItem value={"moderate"}>Moderate</MenuItem>
+                <MenuItem value={"high"}>High</MenuItem>
+              </Select>
+              <FormHelperText>Please select your Risk Index</FormHelperText>
+            </FormControl>
+          </Grid>
+          <Grid item 
+            sx={{
+              ml: "30px",
+              my: "17px"
+            }}
+          >
+            <FormControl>
+              <InputLabel>Investment Goal</InputLabel>
+              <Select
+                value={goal}
+                label="Investment Goal"
+                onChange={e => setGoal(e.target.value)}
+              >
+                <MenuItem value={"RT"}>Retirement</MenuItem>
+                <MenuItem value={"SN"}>Safety Net</MenuItem>
+                <MenuItem value={"BP"}>Big Purchase</MenuItem>
+                <MenuItem value={"GI"}>General Investing</MenuItem>
+              </Select>
+              <FormHelperText>Please select your Investment Goal</FormHelperText>
+            </FormControl>
+          </Grid>
+          <Grid item 
+            alignSelf="end"
+            sx={{
+              mx: "30px",
+              mt: "30px"
+            }}
+          >
+            <Button onSubmit={handleSubmit}
+              variant="contained" 
+              endIcon={<SendIcon/>}
+            >
+              Submit
+            </Button>
+          </Grid>
         </Grid>
       </Container>
     </div>
